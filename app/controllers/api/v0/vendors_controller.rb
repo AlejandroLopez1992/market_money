6be20426c0cbc3b4ee480499e4471a88fafb62e2
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-05-16 11:43:41 -0600

    Feat: happy path impementation for vendor show

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -1,6 +1,11 @@
 class Api::V0::VendorsController < ApplicationController
+  
   def index
     vendors = Market.find(params[:market_id]).vendors
     render json: VendorSerializer.new(vendors)
+  end
+
+  def show
+    render json: VendorSerializer.new(Vendor.find(params[:id]))
   end
 end
--- spec/requests/api/v0/vendors_request_spec.rb
+++ spec/requests/api/v0/vendors_request_spec.rb
@@ -3,9 +3,41 @@ require 'rails_helper'
 describe "Vendors API" do
   describe "GET /api/v0/vendors/:id" do
     it "sends one vendor based on passed ID" do
+      create_vendors = create_list(:vendor, 5)
 
+      get "/api/v0/vendors/#{create_vendors[4].id}"
+
+      vendor5 = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+
+      expect(vendor5[:data]).to have_key(:id)
+      expect(vendor5[:data][:id]).to be_a(String)
+
+      expect(vendor5[:data]).to have_key(:type)
+      expect(vendor5[:data][:type]).to be_a(String)
+      expect(vendor5[:data][:type]).to eq("vendor")
+
+      expect(vendor5[:data]).to have_key(:attributes)
+      expect(vendor5[:data][:attributes]).to be_a(Hash)
+
+      expect(vendor5[:data][:attributes]).to have_key(:name)
+      expect(vendor5[:data][:attributes][:name]).to be_a(String)
+
+      expect(vendor5[:data][:attributes]).to have_key(:description)
+      expect(vendor5[:data][:attributes][:description]).to be_a(String)
+
+      expect(vendor5[:data][:attributes]).to have_key(:contact_name)
+      expect(vendor5[:data][:attributes][:contact_name]).to be_a(String)
+
+      expect(vendor5[:data][:attributes]).to have_key(:contact_phone)
+      expect(vendor5[:data][:attributes][:contact_phone]).to be_a(String)
+     
+      expect(vendor5[:data][:attributes]).to have_key(:credit_accepted)
+      expect(vendor5[:data][:attributes][:credit_accepted]).to eq(true || false)
     end
 
-    it ""
+    it "if input ID is not in database, error is sent" do
+    end
   end
 end
